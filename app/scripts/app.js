88fc88bcda8d22691768874679f20b72422c19d4
Shashank Agrawal <shagrawal99@gmail.com>
2017-04-30 12:26:54 +0530

    Created yo-man angular app

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name wiredepTestApp
+ * @description
+ * # wiredepTestApp
+ *
+ * Main module of the application.
+ */
+angular
+    .module('wiredepTestApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name wiredepTestApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the wiredepTestApp
+ */
+angular.module('wiredepTestApp')
+    .controller('MainCtrl', function () {
+        this.awesomeThings = [
+            'HTML5 Boilerplate',
+            'AngularJS',
+            'Karma'
+        ];
+    });
